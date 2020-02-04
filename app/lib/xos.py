d98ebb8984b3f0b1ec2eccc40d9dafd1852962dc
Harry <harry@boojum.com.au>
2020-02-04 11:14:53 +1100

    #1426 raise on xos http error

--- app/lib/xos.py
+++ app/lib/xos.py
@@ -9,9 +9,11 @@ def update_xos_with_stub_video(video_data):
     xos_video_endpoint = f'{XOS_API_ENDPOINT}assets/'
     headers = {'Authorization': 'Token ' + XOS_AUTH_TOKEN}
     response = requests.post(xos_video_endpoint, json=video_data, headers=headers)
+    response.raise_for_status()
     return response.json()['id']
 
 def update_xos_with_final_video(asset_id, video_data):
     xos_video_endpoint = f'{XOS_API_ENDPOINT}assets/{asset_id}/'
     headers = {'Authorization': 'Token ' + XOS_AUTH_TOKEN}
     response = requests.patch(xos_video_endpoint, json=video_data, headers=headers)
+    response.raise_for_status()
